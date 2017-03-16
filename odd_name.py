cc6588dd6fa5a10da24f1da7662b443b3fe7d3d4
etatt <eriq.tatti@my.jcu.edu.au>
2017-03-16 15:51:34 +1000

    add name add name printing program

--- odd_name.py
+++ odd_name.py
@@ -1,1 +1,13 @@
-'''Eriq 'duh man' Tatti'''
+'''Eriq '' Tatti'''
+
+def main():
+    name = input("please enter your name :")
+
+    while name == "":
+        print("invalid name!!!!!!!!")
+        name = input("please enter your name :")
+
+    for char in range (len(name)):
+        print("{}".format(name[::2]), end=". ")
+
+main()
